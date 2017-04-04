7ae400d9e5683cc4b396eb16b8092a14b3969648
SIDDHARTH DEORE <siddharthdeore@gmail.com>
2017-04-04 16:19:38 +0530

    Create sabertooth.py

---
+++ sabertooth.py
@@ -0,0 +1,41 @@
+import time
+import serial
+#interpolates the variable from input to output range
+def lerp( x, min_in, max_in, min_out, max_out ):
+	return min_out + (x - min_in) * (max_out-min_out)/(max_in-min_in)
+
+#Rotates First Motor conected to M1A M1B of Sabertooth 2X25 or 2X60 Motor Driver
+def driveA( speed ):
+	output = lerp(speed, -100, 100, 0, 127)
+	Sabertooth_Serial.write(output)
+	
+#Rotates Second Motor conected to M2A M2B of Sabertooth 2X25 or 2X60 Motor Driver
+def driveB( speed ):
+	output = lerp(speed, -100, 100, 128, 255)
+	Sabertooth_Serial.write(output)
+	
+try:
+	Sabertooth_Serial = serial.Serial(
+		port='/dev/ttyAMA0',
+		baudrate = 9600,
+		parity=serial.PARITY_NONE,
+		stopbits=serial.STOPBITS_ONE,
+		bytesize=serial.EIGHTBITS,
+		timeout=1
+	)
+	#Sabertooth_Serial = serial.Serial('/dev/ttyACM0',9600)	# Open Serial Port
+	
+	i=0
+	j=0
+	while 1:
+		Sabertooth_Serial.write('%d'%(i))
+		Sabertooth_Serial.write('%d'%(j))
+		time.sleep(100)
+		i += 1
+		j -= 1
+		
+		if (i > 100 or j >100):
+			i,j=j,i
+			
+
+	Sabertooth_Serial.close()								# Close Serial Port
