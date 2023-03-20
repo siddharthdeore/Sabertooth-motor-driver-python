bfb0d5ba9708266a372bcaf646c74599ff2b1664
SIDDHARTH DEORE <siddharth.deore@iit.it>
2023-03-20 11:55:10 +0100

    Update sabertooth.py
    updated fot issue: https://github.com/siddharthdeore/Sabertooth-motor-driver-python/issues/1

--- sabertooth.py
+++ sabertooth.py
@@ -2,17 +2,17 @@ import time
 import serial
 #interpolates the variable from input to output range
 def lerp( x, min_in, max_in, min_out, max_out ):
-	return min_out + (x - min_in) * (max_out-min_out)/(max_in-min_in)
+	return int(min_out + (x - min_in) * (max_out-min_out)/(max_in-min_in))
 
 #Rotates First Motor conected to M1A M1B of Sabertooth 2X25 or 2X60 Motor Driver
 def motorA( speed ):
 	output = lerp(speed, -100, 100, 0, 127)
-	Sabertooth_Serial.write(output)
+	Sabertooth_Serial.write(output.to_bytes(1,'little'))
 	
 #Rotates Second Motor conected to M2A M2B of Sabertooth 2X25 or 2X60 Motor Driver
 def motorB( speed ):
 	output = lerp(speed, -100, 100, 128, 255)
-	Sabertooth_Serial.write(output)
+	Sabertooth_Serial.write(output.to_bytes(1,'little'))
 
 try:
 	# Open Serial Port
